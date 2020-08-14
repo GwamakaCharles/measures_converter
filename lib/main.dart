ac4cdc3c2c95d6ff2248abcb4b85e169fbcea28b
Gwamaka Charles <gwaxart19@gmail.com>
2020-08-14 13:27:52 +0300

    added the convertion functionality as well as the display of the result

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,31 @@ class UnitsConverter extends StatefulWidget {
 class _UnitsConverterState extends State<UnitsConverter> {
   double _numberFrom;
   String _startMeasure;
+  String _convertedMeasure;
+  String _resultMessage;
+
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds(lbs)': 6,
+    'ounces': 7,
+  };
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+
   final List<String> _measures = [
     'meters',
     'kilometers',
@@ -59,8 +84,47 @@ class _UnitsConverterState extends State<UnitsConverter> {
           child: Column(
             children: [
               Spacer(),
+              Text(
+                'Value',
+                style: labelStyle,
+              ),
+              Spacer(),
+              // DropdownButton(
+              //   value: _startMeasure,
+              //   items: _measures
+              //       .map(
+              //         (e) => DropdownMenuItem<String>(
+              //           value: e,
+              //           child: Text(e),
+              //         ),
+              //       )
+              //       .toList(),
+              //   onChanged: (value) {
+              //     setState(() {
+              //       _startMeasure = value;
+              //     });
+              //   },
+              // ),
+              TextField(
+                style: inputStyle,
+                decoration: InputDecoration(
+                    hintText: 'Please insert the measure to be converted'),
+                onChanged: (text) {
+                  var rv = double.tryParse(text);
+                  if (rv != null) {
+                    setState(() {
+                      _numberFrom = rv;
+                    });
+                  }
+                },
+              ),
+              Spacer(),
+              Text(
+                'From',
+                style: labelStyle,
+              ),
               DropdownButton(
-                value: _startMeasure,
+                isExpanded: true,
                 items: _measures
                     .map(
                       (e) => DropdownMenuItem<String>(
@@ -74,24 +138,83 @@ class _UnitsConverterState extends State<UnitsConverter> {
                     _startMeasure = value;
                   });
                 },
+                value: _startMeasure,
               ),
-              TextField(
-                onChanged: (text) {
-                  var rv = double.tryParse(text);
-                  if (rv != null) {
-                    setState(() {
-                      _numberFrom = rv;
-                    });
+              Spacer(),
+              Text(
+                'To',
+                style: labelStyle,
+              ),
+              Spacer(),
+              DropdownButton(
+                isExpanded: true,
+                items: _measures
+                    .map(
+                      (e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(
+                          e,
+                          style: inputStyle,
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _convertedMeasure = value;
+                  });
+                },
+                value: _convertedMeasure,
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  if (_startMeasure.isEmpty ||
+                      _convertedMeasure.isEmpty ||
+                      _numberFrom == 0) {
+                    return;
+                  } else {
+                    convert(_numberFrom, _startMeasure, _convertedMeasure);
                   }
                 },
+                child: Text(
+                  'Convert',
+                  style: inputStyle,
+                ),
+              ),
+              Spacer(
+                flex: 2,
               ),
               Text(
-                (_numberFrom == null) ? '' : _numberFrom.toString(),
+                (_resultMessage == null) ? '' : _resultMessage,
+                style: labelStyle,
+              ),
+              Spacer(
+                flex: 8,
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void convert(double value, String from, String to) {
+    int nFrom = _measuresMap[from];
+    int nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+
+    if (result == 0) {
+      _resultMessage = 'This conversion can not be performed.';
+    } else {
+      _resultMessage =
+          '${_numberFrom.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
   }
 }
